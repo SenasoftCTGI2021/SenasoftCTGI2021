929fa2385d8e4d45104eb8a25f9446c298ab5fc3
unknown <User1@MEDDFPREAGOP223.FormacionSena.RED>
2021-10-20 12:56:18 -0500

    Primer push de back-end

---
+++ Frontend/database/login.php
@@ -0,0 +1,19 @@
+<?php
+
+    require_once "conexion.php";
+
+
+    if (isset($_POST['boton_ingreso'])) {
+        $username = $_POST['nombre'];
+        $contrasenna = $_POST['ingresoPassword'];
+        
+        $sql = "SELECT * FROM jugadores WHERE username_jugador='$username' AND password_jugador='$contrasenna'";
+        $result = mysqli_query($conn, $sql);
+        
+        if ($result->num_rows>0) {
+            echo '<script language="javascript">alert("Inicio de sesion correcto");window.location.href="../paginas/index.php"</script>';
+        }else {
+            echo '<script language="javascript">alert("Usuario y/o contraseña incorrectos");window.location.href="../paginas/ingreso.php"</script>';
+        }
+    }
+?>
